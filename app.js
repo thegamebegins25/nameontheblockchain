4bb50dcecf97e1f117ac003ad42337a9398f0588
thegamebegins25 <wdppostcard@gmail.com>
2022-04-02 13:56:11 -0400

    test

--- app.js
+++ app.js
@@ -9,12 +9,11 @@ sendEthButton.addEventListener('click', () => {
     nonce: '0x00', // ignored by MetaMask
     gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
     gas: '0x2710', // customizable by user during MetaMask confirmation.
-    to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+    to: '0x61a80784e389563cdc1027012d4D5d7b83Defc5e', // Required except during contract publications.
     from: ethereum.selectedAddress, // must match user's active address.
-    value: '0x00', // Only required to send ether to the recipient from the initiating external account.
-    data:
-      '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
-    chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+    value: '0x01', // Only required to send ether to the recipient from the initiating external account.
+    //data:
+      //'', // Optional, but used for defining smart contract creation and interaction.
   };
   ethereum
     .request({
