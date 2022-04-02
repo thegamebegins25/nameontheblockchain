e055819110bcd8ad52cc6980122eec523b44629a
thegamebegins25 <wdppostcard@gmail.com>
2022-04-02 13:21:23 -0400

    params

--- app.js
+++ app.js
@@ -1,6 +1,35 @@
 const ethereumButton = document.querySelector('.enableEthereumButton');
+const sendEthButton = document.querySelector('.sendEthButton');
+
+let accounts = [];
+
+const transactionParameters = {
+    nonce: '0x00', // ignored by MetaMask
+    gasPrice: '0x09184e72a000', // customizable by user during MetaMask confirmation.
+    gas: '0x2710', // customizable by user during MetaMask confirmation.
+    to: '0x0000000000000000000000000000000000000000', // Required except during contract publications.
+    from: ethereum.selectedAddress, // must match user's active address.
+    value: '0x00', // Only required to send ether to the recipient from the initiating external account.
+    data:
+      '0x7f7465737432000000000000000000000000000000000000000000000000000000600057', // Optional, but used for defining smart contract creation and interaction.
+    chainId: '0x3', // Used to prevent transaction reuse across blockchains. Auto-filled by MetaMask.
+  };
+
+//Sending Ethereum to an address
+sendEthButton.addEventListener('click', () => {
+  ethereum
+    .request({
+      method: 'eth_sendTransaction',
+      params: [tparams],
+    })
+    .then((txHash) => console.log(txHash))
+    .catch((error) => console.error);
+});
 
 ethereumButton.addEventListener('click', () => {
-  //Will Start the metamask extension
-  ethereum.request({ method: 'eth_requestAccounts' });
+  getAccount();
 });
+
+async function getAccount() {
+  accounts = await ethereum.request({ method: 'eth_requestAccounts' });
+}
