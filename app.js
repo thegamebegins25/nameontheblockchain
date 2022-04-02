2aa24dd513e712dad9b32b260e21443e9d339859
thegamebegins25 <wdppostcard@gmail.com>
2022-04-02 15:47:08 -0400

    gas2

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ let accounts = [];
 sendEthButton.addEventListener('click', () => {
   const tparams = {
     nonce: '0x00', // ignored by MetaMask
+    gas: '0x2710', // customizable by user during MetaMask confirmation.
     to: '0x61a80784e389563cdc1027012d4D5d7b83Defc5e', // Required except during contract publications.
     from: ethereum.selectedAddress, // must match user's active address.
     value: '0xDE0B6B3A7640000', // Only required to send ether to the recipient from the initiating external account.
