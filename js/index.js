db58f3136eba7db9f8553403b5ad2693770e393b
thegamebegins25 <wdppostcard@gmail.com>
2022-04-07 20:06:47 -0400

    basic design

--- js/index.js
+++ js/index.js
@@ -1,7 +1,12 @@
 function eternalize() {
 
     let inputVal = document.getElementById("nameinput").value;
+    console.log(inputVal);
+    if(inputVal) {
+      let link = "pay.html?name=" + inputVal
+      window.location.href = link;
+      return
+    }
 
-    let link = "pay.html?name=" + inputVal
-    window.location.href = link;
+    alert('Please enter a name.');
   }
--- js/pay.js
+++ js/pay.js
@@ -1,8 +1,5 @@
-const paypal = document.querySelector('.paypal');
-const metamask = document.querySelector('.metamask');
-
 //Sending Ethereum to an address
-metamask.addEventListener('click', () => {
+function metamask() {
     const urlSearchParams = new URLSearchParams(window.location.search);
     const params = Object.fromEntries(urlSearchParams.entries());
     let name = params['name'];
@@ -13,4 +10,17 @@ metamask.addEventListener('click', () => {
 
     let link = "paymetamask.html?name=" + name
     window.location.href = link;
-});
+};
+
+function paypal() {
+    const urlSearchParams = new URLSearchParams(window.location.search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    let name = params['name'];
+
+    if(typeof name === 'undefined') {
+        alert('Name is undefined. Please go to the home page and enter a name.');
+      }
+
+    let link = "paypaypal.html?name=" + name
+    window.location.href = link;
+};
