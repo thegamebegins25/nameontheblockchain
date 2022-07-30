b610c2d9ffe199ffb0373ff74c30e4a51e3c1e03
thegamebegins25 <wdppostcard@gmail.com>
2022-07-29 21:02:06 -0400

    added paypal

--- js/paypaypal.js
+++ js/paypaypal.js
@@ -1,13 +1,9 @@
 const urlSearchParams = new URLSearchParams(window.location.search);
 const params = Object.fromEntries(urlSearchParams.entries());
-let name = params['name'];
+window.name = params['name'];
 
 const namehtml = document.getElementById('name');
 
 
-let text = "Confirm your name: " + name;
+let text = "Confirm your name: " + window.name;
 namehtml.textContent = text;
-
-function checkout() {
-    console.log("Checkout");
-};
